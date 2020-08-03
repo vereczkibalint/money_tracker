b0da80e77daf77bd6f4ec51e6c1c7d9309cd2fae
vereczkibalint <vereczkibalint@gmail.com>
2020-08-03 14:13:46 +0200

    added auth login route

---
+++ server/routes/api/auth.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+
+const { loginUser } = require('../../services/AuthService');
+
+const { check, validationResult } = require('express-validator');
+
+router.post('/',
+  check('email', 'Email megadása kötelező!').isEmail(),
+  check('password', 'Jelszó megadása kötelező!').isLength({ min: 6 })
+, (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+      return res.status(400).json({ status_code: 'ERR_VALIDATION_ERROR', errors: errors });
+  }
+
+  const { email, password } = req.body;
+
+  const user = {
+    email,
+    password
+  };
+
+  loginUser(user, (error) => {
+    return res.status(400).json(error);
+  }, (result) => {
+    return res.json(result);
+  });
+
+
+});
+
+module.exports = router;
