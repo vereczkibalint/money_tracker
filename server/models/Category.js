3e6532851a41945668928ec1c82e2062b5ce1c83
vereczkibalint <vereczkibalint@gmail.com>
2020-08-03 14:13:24 +0200

    changed schema, added creator user field

--- server/models/Category.js
+++ server/models/Category.js
@@ -6,7 +6,11 @@ const hexaColorValidator = (value) => (/^#([0-9a-f]{3}){1,2}$/i).test(value);
 
 const CategorySchema = new Schema({
     name: String,
-    color: String
+    color: String,
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    }
 });
 
 module.exports = mongoose.model('category', CategorySchema);
