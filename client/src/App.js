1d010d99e4386a5df70533fc1ea5b2c74e7a1f2d
vereczkibalint <vereczkibalint@gmail.com>
2020-08-04 12:57:02 +0200

    initial react commit

--- client/src/App.js
+++ client/src/App.js
@@ -6,26 +6,9 @@ import store from './store';
 window.store = store;
 
 const App = () => {
-  useEffect(() => {
-    axios.post('/api/auth', {
-	      email: 'vereczkibalint@gmail.com', 
-        password: 'asdasda'
-        }, 
-        {
-          headers: {
-              'Content-Type': 'application/json; charset=UTF-8'
-          }
-        })
-        .then(response => { 
-          console.log(response)
-        })
-        .catch(error => {
-            console.log(error.response)
-        });
-  }, []);
   return (
     <div className="App">
-      
+      Hello World
     </div>
   );
 }
