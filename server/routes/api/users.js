00237667b06cf2898adee6e958147de62e3f7417
vereczkibalint <vereczkibalint@gmail.com>
2020-08-04 09:07:59 +0200

    removed unnecessary bcrypt import

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
 
 const { check, validationResult } = require('express-validator');
