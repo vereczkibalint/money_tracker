3300a9f3fc8dc9fe0405045cbe0f3fbd2f808ba4
vereczkibalint <vereczkibalint@gmail.com>
2020-08-04 09:51:23 +0200

    money schema created

---
+++ server/models/Money.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const MoneySchema = new Schema({
+  ownedBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  moneyType: {
+    type: String,
+    enum: ['income', 'expense'],
+    required: true
+  },
+  amount: {
+    type: Number,
+    min: 1,
+    required: true
+  },
+  issueDate: {
+    type: Date,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('money', MoneySchema);
