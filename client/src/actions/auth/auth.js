b1b708533f7152475daebb0b90216c0666460c1c
vereczkibalint <vereczkibalint@gmail.com>
2020-08-10 19:27:35 +0200

    implemented action to fetch API with login data and manage authentication

--- client/src/actions/auth/auth.js
+++ client/src/actions/auth/auth.js
@@ -1,23 +1,48 @@
 import * as actions from './types';
+import axios from 'axios';
 
-export const beginLogin = (credentials) => ({
-  type: actions.BEGIN_LOGIN,
-  payload: {
-    credentials
+const API_PATH = "/api/auth";
+const jsonHeader = {
+  'Content-Type': 'application/json'
+};
+
+export const beginLogin = () => {
+  return {
+    type: actions.BEGIN_LOGIN
   }
-});
+}
 
-export const loginSuccessful = (token) => ({
+export const login = (credentials) => {
+  return function (dispatch) {
+    try {
+      const { email, password } = credentials;
+      axios.post(API_PATH, { email, password }, {
+        headers: jsonHeader
+      }).then(res => {
+          dispatch(loginSuccessful(res.data.token, res.data.user));
+      }).catch(err => {
+        if(err.response.status === 400) {
+          dispatch(loginFailed(err.response.data.errors));
+        }
+      });
+    } catch (err) {
+      dispatch(loginFailed([err.message]));
+    }
+  }
+}
+
+export const loginSuccessful = (token, user) => ({
   type: actions.LOGIN_SUCCESSFUL,
   payload: {
-    token
+    token,
+    user
   }
 });
 
-export const loginFailed = (errorMessage) => ({
+export const loginFailed = (errors) => ({
   type: actions.LOGIN_FAILED,
   payload: {
-    message: errorMessage
+    errors
   }
 });
 
