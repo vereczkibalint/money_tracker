5e8b575e99240bb82a8ef1bb325d8831367e0e71
vereczkibalint <vereczkibalint@gmail.com>
2020-08-10 13:02:10 +0200

    added route paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import { Switch, Route } from 'react-router-dom';
+
 import Sidebar from './components/layout/Sidebar';
 import HomePage from './components/HomePage';
 
 import ActionButton from './components/layout/ActionButton';
 import Login from './components/Login';
+import Register from './components/Register';
+import LostPassword from './components/LostPassword';
 
 const App = () => {
   return (
@@ -15,7 +19,13 @@ const App = () => {
         <div className="content">
           { /* <HomePage /> */}
           { /* <ActionButton /> */ }
-          <Login />
+          <Switch>
+            <Route path="/" exact component={Login} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/register" exact component={Register} />
+            <Route path="/password_recovery" exact component={LostPassword} />
+            <Route path="/dashboard" exact component={HomePage} />
+          </Switch>
         </div>
       </div>
     </div>
