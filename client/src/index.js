046ed9cb5f7ac06df83ac22be3fe5ddc93798b5e
vereczkibalint <vereczkibalint@gmail.com>
2020-08-10 10:47:34 +0200

    provider added with basic store for testing

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
