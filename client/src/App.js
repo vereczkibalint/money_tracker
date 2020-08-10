ab46f330c589cda284fc0b2b685b688b6c5ddffe
vereczkibalint <vereczkibalint@gmail.com>
2020-08-10 19:26:51 +0200

    added private route for dashboard and selector to get logged in user from state

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './App.css';
 
+import { useSelector } from 'react-redux';
+
 import { Switch, Route } from 'react-router-dom';
 
 import Sidebar from './components/layout/Sidebar';
-import HomePage from './components/HomePage';
+import HomePage from './components/pages/HomePage';
+
+import PrivateRoute from './components/PrivateRoute';
 
 import ActionButton from './components/layout/ActionButton';
-import Login from './components/Login';
-import Register from './components/Register';
-import LostPassword from './components/LostPassword';
+import Login from './components/pages/Login';
+import Register from './components/pages/Register';
+import LostPassword from './components/pages/LostPassword';
 
 const App = () => {
+  const user = useSelector(state => state.auth.currentUser);
+
   return (
     <div className="App">
       <div className="layout">
@@ -24,7 +30,7 @@ const App = () => {
             <Route path="/login" exact component={Login} />
             <Route path="/register" exact component={Register} />
             <Route path="/password_recovery" exact component={LostPassword} />
-            <Route path="/dashboard" exact component={HomePage} />
+            <PrivateRoute path="/dashboard" exact component={HomePage} authUser={user} />
           </Switch>
         </div>
       </div>
