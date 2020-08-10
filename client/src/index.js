84048f6c6c2e6dbaed833b5b68ac0207e7fa34cc
vereczkibalint <vereczkibalint@gmail.com>
2020-08-10 12:58:42 +0200

    added router

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,17 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
