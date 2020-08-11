dfee580dae605b09661bc11668aef8dbe505f047
vereczkibalint <vereczkibalint@gmail.com>
2020-08-11 09:23:45 +0200

    header now renders accordingly to store user object (authenticated)

--- client/src/components/layout/SidebarHeader.js
+++ client/src/components/layout/SidebarHeader.js
@@ -1,25 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
+import { useSelector } from 'react-redux';
 
 import './SidebarHeader.css';
 import profileImg from '../../assets/images/profile.png';
 
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+
 const SidebarHeader = () => {
-    const [username, setUsername] = useState('Teszt');
+    const userObject = localStorage.getItem('user');
+    const user = JSON.parse(userObject);
 
     return (
         <div className="sidebar-header">
-            <div className="user-info">
-                <span className="user-img">
-                    <img src={profileImg} alt="profilkép" />
-                </span>
-                <span className="user-welcome">
-                    Üdvözlünk, {username}!
-                </span>
-            </div>
-            <div className="logout">
-                <button type="button" className="btn-logout">Kilépés</button>
-            </div>
-        </div> 
+            {user !== null ? (
+                <Fragment>
+                    <div className="user-info">
+                        <span className="user-img">
+                            <img src={profileImg} alt="profilkép" />
+                        </span>
+                        <span className="user-welcome">
+                            Üdvözlünk, {user.firstName}!
+                        </span>
+                    </div>
+                    <div className="logout">
+                        <Link to="/logout">
+                            <Button variant="danger" size="sm" className="mb-2">Kilépés</Button>
+                        </Link>
+                    </div>
+                </Fragment>
+            ) : (
+                <Fragment>
+                    <div className="user-info">
+                        <span className="user-img">
+                            <img src={profileImg} alt="profilkép" />
+                        </span>
+                    </div>
+                    <div className="login">
+                        <Link to="/login">
+                            <Button variant="danger" size="sm" className="mb-2">Bejelentkezés</Button>
+                        </Link>
+                    </div>
+                </Fragment>
+            )}
+        </div>
     );
 }
 
