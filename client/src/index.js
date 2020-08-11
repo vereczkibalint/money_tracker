6aa43bca61b8a33de52344170b583214106d7236
vereczkibalint <vereczkibalint@gmail.com>
2020-08-11 09:21:04 +0200

    moved router from index.js to App component

--- client/src/index.js
+++ client/src/index.js
@@ -4,17 +4,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
-import { BrowserRouter as Router } from 'react-router-dom';
-
 import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
       <App />
-    </Router>
   </Provider>,
   document.getElementById('root')
 );
