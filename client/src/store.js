99eb92b043940d9178613f0dca90d523b7c3e957
vereczkibalint <vereczkibalint@gmail.com>
2020-08-24 16:58:41 +0200

    redux devtools enabled

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
@@ -10,7 +10,9 @@ const initialState = {};
 const store = createStore(
   rootReducer,
   initialState,
-  applyMiddleware(thunk)
+  compose(
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
 
 window.store = store;
