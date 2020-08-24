49e3f4ea006ca43971fe0a7deeddffeaccf85563
vereczkibalint <vereczkibalint@gmail.com>
2020-08-24 16:59:44 +0200

    created ui for expense listing and filtering

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,30 +1,66 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+import './Dashboard.css';
+import "react-datepicker/dist/react-datepicker.css";
 
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { fetchAllExpenses } from '../../services/expenseService';
+import { fetchAllExpenses, filterExpensesBySearchTerm } from '../../services/expenseService';
+import { Form } from 'react-bootstrap';
+
+import DatePicker from 'react-datepicker';
 
 import MoneyCard from '../money-card/MoneyCard';
 
-const Dashboard = ({ expenses, fetchAllExpenses }) => {
+const Dashboard = ({ expenses, fetchAllExpenses, filterExpensesBySearchTerm }) => {
+
+  const [searchTerm, setSearchTerm] = useState('');
   
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const onChange = dates => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
+
   useEffect(() => {
     fetchAllExpenses();
-  }, [fetchAllExpenses]);
+  }, []);
 
   return (
-    <div>
-      <h2 className="text-center mt-2">Az összes pénzmozgás</h2>
-      {expenses.map(moneyData => {
+    <Fragment>
+      <h2 className="text-center mt-3">Összegzés</h2>
+      <div className="expense-filter">
+        <div className="mt-3 dateFilter" style={{margin: '0 auto'}}>
+          <DatePicker
+            selected={startDate}
+            onChange={onChange}
+            startDate={startDate}
+            endDate={endDate}
+            selectsRange
+            inline
+          />
+          <div className="filters">
+          <Form.Control type="text" style={{marginBottom: '5px'}} placeholder="Keresendő kifejezés..." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+          <Form.Control as="select">
+            <option value="income">Bevétel</option>
+            <option value="expense">Kiadás</option>
+          </Form.Control>
+        </div>
+        </div>
+      </div>
+      {expenses.length > 0 && expenses.map(moneyData => {
           return <MoneyCard moneyData={moneyData} key={moneyData._id} />;
       })}
-    </div>
+    </Fragment>
   )
 }
 
 Dashboard.propTypes = {
   fetchAllExpenses: PropTypes.func.isRequired,
+  filterExpensesBySearchTerm: PropTypes.func.isRequired,
+  searchTerm: PropTypes.string,
   expenses: PropTypes.array
 };
 
@@ -32,4 +68,4 @@ const mapStateToProps = state => ({
   expenses: state.expenses.expenses
 });
 
-export default connect(mapStateToProps, { fetchAllExpenses })(Dashboard);
+export default connect(mapStateToProps, { fetchAllExpenses, filterExpensesBySearchTerm })(Dashboard);
