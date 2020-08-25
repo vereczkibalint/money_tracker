99854159ab11d0c7f81afa4996ff589340eba0e0
vereczkibalint <vereczkibalint@gmail.com>
2020-08-25 10:13:39 +0200

    material ui theme change to match primary color with the site

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
 import './assets/styles/colors.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min';
@@ -11,9 +13,19 @@ import 'popper.js/dist/popper.min';
 import { Provider } from 'react-redux';
 import store from './store';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#4e7302',
+    },
+  }
+});
+
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <MuiThemeProvider theme={theme}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
